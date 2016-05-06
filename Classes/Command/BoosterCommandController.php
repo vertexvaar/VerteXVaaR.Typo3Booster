ffb16b332634f114673ae8d67e9087a9b0b4585c
Oliver Eglseder <php@vxvr.de>
2016-05-06 14:55:22 +0200

    [TASK] Do not recreate class preload file without force

--- Classes/Command/BoosterCommandController.php
+++ Classes/Command/BoosterCommandController.php
@@ -1,6 +1,9 @@
 <?php
 namespace VerteXVaaR\Booster\Command;
 
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 use VerteXVaaR\Booster\Service\ClassPreLoadService;
 
@@ -15,26 +18,46 @@ class BoosterCommandController extends CommandController
     protected $classPreLoadService = null;
 
     /**
+     * @var FrontendInterface
+     */
+    protected $cache = null;
+
+    /**
      * BoosterCommandController constructor.
      */
     public function __construct()
     {
         $this->classPreLoadService = new ClassPreLoadService();
+        $this->cache = GeneralUtility::makeInstance(CacheManager::class)->getCache('cache_preload');
     }
 
     /**
-     *
+     * @param bool $force
      */
-    public function frontendCommand()
+    public function frontendCommand($force = false)
     {
-        $this->classPreLoadService->generate('Frontend');
+        $this->run($force, 'Frontend');
     }
 
     /**
-     *
+     * @param bool $force
      */
-    public function backendCommand()
+    public function backendCommand($force = false)
     {
-        $this->classPreLoadService->generate('Backend');
+        $this->run($force, 'Backend');
+    }
+
+    /**
+     * @param bool $force
+     * @param string $context
+     */
+    protected function run($force, $context)
+    {
+        if (true === $force || !$this->cache->has($context)) {
+            $this->outputLine(sprintf('Generating class preload file for %s', $context));
+            $this->classPreLoadService->generate($context);
+        } else {
+            $this->outputLine('Skipping preload because it\'s already present. Use --force to overwrite it.');
+        }
     }
 }
